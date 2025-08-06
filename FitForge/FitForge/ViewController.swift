97fc25d73a76f527dc80fe7aa506b11fc279e27c
Rezwan Mahmud <rezwanmahmud@Mac.lan>
2025-08-06 01:58:11 -0400

    chore: initial Xcode project scaffold for FitForge

---
+++ FitForge/FitForge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitForge
+//
+//  Created by Rezwan Mahmud on 8/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
